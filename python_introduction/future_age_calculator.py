864cefe1a91350c7c3a26b6b9bc8c322fe776076
alainguillaume <manishalain@hotmail.com>
2024-09-16 05:14:34 +0200

    fixing age calculation

--- python_introduction/future_age_calculator.py
+++ python_introduction/future_age_calculator.py
@@ -1,6 +1,6 @@
-age =int(input("How old are you ?"))
+age = int(input("How old are you ?"))
 
 
 
 
-print("In 2050, you will be ",age+27, " years old.")
+print("In 2050, you will be ",age + 27, " years old.")
