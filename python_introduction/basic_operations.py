31177dbf38ea7ba6d5b5a5b3a9986b5eee0dfca0
alainguillaume <manishalain@hotmail.com>
2024-09-16 03:37:04 +0200

    performing arthimetic oparations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,6 @@
+num1=10
+num2=5
+
+print("Addition of ",num1, "and ", num2, "is ", num1+num2 )
+print("Substraction of ",num1, "and ", num2, "is ", num1-num2 )
+print("Multiplication of ",num1, "and ", num2, "is ", num1*num2 )
