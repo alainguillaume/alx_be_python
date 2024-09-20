5d83d889b31575976f820f98cb47062ca1fd9d3a
alainguillaume <manishalain@hotmail.com>
2024-09-19 17:10:16 -0700

    adding patter

---
+++ control-flow/pattern_drawing.py
@@ -0,0 +1,10 @@
+rows = int(input("Enter the size of the pattern:"))
+i = 0
+while i < rows:
+        j = 0
+        while j < rows:
+                print("*", end="")
+                j += 1
+        
+        i += 1
+        print()
