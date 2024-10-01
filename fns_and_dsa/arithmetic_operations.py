2fdbf2693a7ea36f80393093f5898e6ff32a0830
alainguillaume <manishalain@hotmail.com>
2024-10-02 01:46:16 +0200

    arthimetic-operation-funtion-import

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,16 @@
+result = 0
+def perform_operation(num1, num2, operation):
+    if operation == 'add':
+        result = num1 + num2
+    elif operation == 'subtract':
+        result = num1 - num2
+    elif operation == 'multiply':
+        result = num1 * num2
+    elif operation == 'divide':
+        if num2 > 0:
+            result = num1 / num2
+        else:
+            return 0
+    return result
+
+
