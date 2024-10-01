87e1fdc144b9c28135a600a4a63295e6918a380e
alainguillaume <manishalain@hotmail.com>
2024-10-02 01:49:46 +0200

    validating-0-on-division

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -7,7 +7,7 @@ def perform_operation(num1, num2, operation):
     elif operation == 'multiply':
         result = num1 * num2
     elif operation == 'divide':
-        if num2 > 0:
+        if num2 == 0:
             result = num1 / num2
         else:
             return 0
