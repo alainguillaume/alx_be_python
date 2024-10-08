f99b8a4a4cbc27bb93242962f3e38574aa07f7a9
alainguillaume <manishalain@hotmail.com>
2024-10-08 12:47:09 +0200

    function naming#1

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -12,7 +12,7 @@ class TestSimpleCalculator(unittest.TestCase):
          self.assertEqual(self.calc.add(-1, -1), -2)
 
 
-    def test_subStraction(self):
+    def test_subtraction(self):
         self.assertEqual(self.calc.subtract(5, 3), 2)
         self.assertEqual(self.calc.subtract(-1, 1), -2)
         self.assertEqual(self.calc.subtract(-1, -1), 0)
