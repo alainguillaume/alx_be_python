c7c6638e2fd03644e763fb54457f4205922ec550
alainguillaume <manishalain@hotmail.com>
2024-10-08 12:46:13 +0200

    function naming

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -6,13 +6,13 @@ class TestSimpleCalculator(unittest.TestCase):
         """Set up the SimpleCalculator instance before each test."""
         self.calc = SimpleCalculator()
 
-    def test_add(self):
+    def test_addition(self):
          self.assertEqual(self.calc.add(2, 3), 5)
          self.assertEqual(self.calc.add(-1, 1), 0)
          self.assertEqual(self.calc.add(-1, -1), -2)
 
 
-    def test_subStract(self):
+    def test_subStraction(self):
         self.assertEqual(self.calc.subtract(5, 3), 2)
         self.assertEqual(self.calc.subtract(-1, 1), -2)
         self.assertEqual(self.calc.subtract(-1, -1), 0)
