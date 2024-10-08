e1e45f040454d5e09bb48f3ad2ca6651f15af965
alainguillaume <manishalain@hotmail.com>
2024-10-08 07:47:25 +0200

    adding-classes

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,24 @@
+class BankAccount:
+    def __init__(self, account_balance):
+        self.account_balance = account_balance
+
+    def deposit(self, amount):
+        self.account_balance +=  amount
+        return self.account_balance
+  
+        
+    def withdraw(self,amount):
+          if self.account_balance <  amount:
+             self.account_balance -= amount
+             return self.account_balance
+        
+    def display_balance(self):
+          print(f"Current Balance: {self.account_balance}")
+
+        
+
+
+
+
+
+        
